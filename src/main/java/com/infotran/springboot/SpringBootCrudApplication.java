865008dd87578307c70c940a41f84f615db7baf6
Hsieh <a0936110466@gmail.com>
2021-11-03 17:55:16 +0800

    123

---
+++ src/main/java/com/infotran/springboot/SpringBootCrudApplication.java
@@ -0,0 +1,24 @@
+package com.infotran.springboot;
+
+import javax.servlet.Filter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@SpringBootApplication
+public class SpringBootCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudApplication.class, args);
+	}
+	
+	@Bean 
+	FilterRegistrationBean<Filter>  hiddenHttpMethodFilter(){
+		FilterRegistrationBean<Filter>  filterBean = new FilterRegistrationBean<>();
+		filterBean.setFilter(new HiddenHttpMethodFilter());
+		return filterBean;
+	}
+}
